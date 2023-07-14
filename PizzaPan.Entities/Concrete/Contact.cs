5bc20f0134ffc2ad83271383e67f82ff5c30cdf2
Gokhan <engokhangok@gmail.com>
2023-07-14 08:47:58 +0300

    Entities Nesneleri Oluşturuldu.

---
+++ PizzaPan.Entities/Concrete/Contact.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace PizzaPan.Entities.Concrete
+{
+    public class Contact
+    {
+        public int ContactID { get; set; }
+        public string Name { get; set; }
+        public string Subject { get; set; }
+        public string Email { get; set; }
+        public string Message { get; set; }
+        public DateTime SendDateMessage { get; set; }
+    }
+}
---
+++ PizzaPan.Entities/Concrete/Testimonial.cs
@@ -0,0 +1,11 @@
+﻿namespace PizzaPan.Entities.Concrete
+{
+    public class Testimonial
+    {
+        public int TestimonialID { get; set; }
+        public string CustomerName { get; set; }
+        public string CustomerTitle { get; set; }
+        public string Comment { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
