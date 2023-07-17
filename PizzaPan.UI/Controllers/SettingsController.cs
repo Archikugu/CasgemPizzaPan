fe48c27c407edf20f3c5c818fe0f46c3d89dcc44
Gokhan <engokhangok@gmail.com>
2023-07-17 11:39:17 +0300

    Profil Sayfası Güncelleme eklendi ve giriş yap hatası düzeltildi.

---
+++ PizzaPan.UI/Controllers/SettingsController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using PizzaPan.Entities.Concrete;
+using PizzaPan.UI.Models;
+using System.Threading.Tasks;
+
+namespace PizzaPan.UI.Controllers
+{
+    public class SettingsController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public SettingsController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var value = await _userManager.FindByNameAsync(User.Identity.Name);
+            UserEditViewModel model = new UserEditViewModel();
+            model.Name = value.Name;
+            model.Surname = value.Surname;
+            model.Email = value.Email;
+            model.City = value.City;
+            model.Username = value.UserName;
+            return View(model);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Index(UserEditViewModel userEditViewModel)
+        {
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+            user.Surname = userEditViewModel.Surname;
+            user.Name = userEditViewModel.Name;
+            user.City = userEditViewModel.City;
+            user.Email = userEditViewModel.Email;
+            user.PasswordHash = _userManager.PasswordHasher.HashPassword(user, userEditViewModel.Password);
+            var result = await _userManager.UpdateAsync(user);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index", "SignIn");
+            }
+            return View();
+        }
+    }
+}
