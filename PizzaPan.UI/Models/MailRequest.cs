5a0bd7901d952c49fa71d37d1939921e2828b76f
Gokhan <engokhangok@gmail.com>
2023-07-17 12:22:09 +0300

    Mail Gönderme İşlemi Eklendi

---
+++ PizzaPan.UI/Models/MailRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace PizzaPan.UI.Models
+{
+    public class MailRequest
+    {
+        public string ReceiverMail { get; set; }
+        public string Subject { get; set; }
+        public string MessageContent { get; set; }
+    }
+}
