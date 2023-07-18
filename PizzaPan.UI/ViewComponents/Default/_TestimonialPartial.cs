8686f8d2ffd1cccd49fd71cdc85b848114ea839e
Gokhan <engokhangok@gmail.com>
2023-07-18 11:26:25 +0300

    Tema Partial'lara Bölündü.

---
+++ PizzaPan.UI/ViewComponents/Default/_TestimonialPartial.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PizzaPan.Business.Abstract;
+
+namespace PizzaPan.UI.ViewComponents.Default
+{
+    public class _TestimonialPartial : ViewComponent
+    {
+        private readonly ITestimonialsService _testimonialsService;
+
+        public _TestimonialPartial(ITestimonialsService testimonialsService)
+        {
+            _testimonialsService = testimonialsService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var values = _testimonialsService.TGetList();
+            return View(values);
+        }
+    }
+}
