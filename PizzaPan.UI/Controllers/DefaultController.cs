d7f81c1012a6dd8c8370d798994a976764ea6bfc
Gokhan <engokhangok@gmail.com>
2023-07-22 06:33:33 +0300

    Revize Edildi

--- PizzaPan.UI/Controllers/DefaultController.cs
+++ PizzaPan.UI/Controllers/DefaultController.cs
@@ -1,9 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PizzaPan.DataAccess.Concrete;
 
 namespace PizzaPan.UI.Controllers
 {
     public class DefaultController : Controller
     {
+        private readonly Context _context;
+
+        public DefaultController(Context context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
--- PizzaPan.UI/ViewComponents/Default/_ProductPartial.cs
+++ PizzaPan.UI/ViewComponents/Default/_ProductPartial.cs
@@ -1,12 +1,13 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using PizzaPan.Business.Abstract;
+using PizzaPan.Business.Concrete;
+using PizzaPan.DataAccess.EntityFramework;
 
 namespace PizzaPan.UI.ViewComponents.Default
 {
     public class _ProductPartial : ViewComponent
     {
         private readonly IProductService _productService;
-
         public _ProductPartial(IProductService productService)
         {
             _productService = productService;
