9ac35cb54f193544171df73d1748494e4abf4cf5
Gokhan <engokhangok@gmail.com>
2023-07-22 13:03:54 +0300

    About Sayfası Eklendi

--- PizzaPan.UI/Controllers/AboutController.cs
+++ PizzaPan.UI/Controllers/AboutController.cs
@@ -1,12 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PizzaPan.Business.Abstract;
 
 namespace PizzaPan.UI.Controllers
 {
     public class AboutController : Controller
     {
+        private readonly IOurTeamService _ourTeamService;
+
+        public AboutController(IOurTeamService ourTeamService)
+        {
+            _ourTeamService = ourTeamService;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var values = _ourTeamService.TGetList();
+            return View(values);
         }
     }
 }
