1f9e3555b0b25b9789aa9385858a2cb4f616894e
Gokhan <engokhangok@gmail.com>
2023-07-22 13:36:58 +0300

    İletişim Kısmı Eklendi ve Revizeler Eklendi.

---
+++ PizzaPan.UI/Controllers/ContactController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PizzaPan.Business.Abstract;
+using PizzaPan.DataAccess.Concrete;
+using PizzaPan.Entities.Concrete;
+
+namespace PizzaPan.UI.Controllers
+{
+    public class ContactController : Controller
+    {
+        private readonly IContactService _contactService;
+
+        public ContactController(IContactService contactService)
+        {
+            _contactService = contactService;
+
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(Contact context)
+        {
+            _contactService.TInsert(context);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ PizzaPan.UI/Controllers/MenuController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PizzaPan.Business.Abstract;
+using PizzaPan.DataAccess.Concrete;
+using System.Linq;
+
+namespace PizzaPan.UI.Controllers
+{
+    public class MenuController : Controller
+    {
+        private readonly IProductService _productService;
+        private readonly Context _context;
+
+        public MenuController(IProductService productService, Context context)
+        {
+            _productService = productService;
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var values = _context.Categories.Include(x => x.Products).ToList();
+            return View(values);
+        }
+    }
+}
